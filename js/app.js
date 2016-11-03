d4aebf19325ebcd04a1a223afb9b7dec18b88371
Cameron Skidmore <cskidz99@Camerons-MacBook-Air.local>
2016-11-02 18:05:30 -0600

    Photoswipe gallery working now

---
+++ js/app.js
@@ -0,0 +1,62 @@
+'use strict';
+
+angular
+  .module('demoApp', ['ngPhotoswipe'])
+  .controller('demoCtrl', demoCtrlFn);
+
+function demoCtrlFn() {
+  var vm = this;
+
+  vm.title = 'Test Gallery';
+
+  vm.opts = {
+    index: 0
+  };
+
+  vm.slides = [{
+      src: 'http://lorempixel.com/500/500/business/1',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/2',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/3',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/4',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/5',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/6',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/7',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/8',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/9',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business/10',
+      w: 500, h: 500
+    }];
+
+  vm.showGallery = function (i) {
+    if(angular.isDefined(i)) {
+      vm.opts.index = i;
+    }
+    vm.open = true;
+  };
+
+  vm.closeGallery = function () {
+    vm.open = false;
+  };
+}
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['demoApp']);
+});
---
+++ js/photoswipe-constructor.js
@@ -0,0 +1,38 @@
+var openPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
+            w: 964,
+            h: 1024
+        },
+        {
+            src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
+            w: 1024,
+            h: 683
+        }
+    ];
+
+    // define options (if needed)
+    var options = {
+        // optionName: 'option value'
+        // for example:
+        index: 0 // start at first slide
+        // history & focus options are disabled on CodePen
+        history: false,
+        focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+
+    };
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+
+openPhotoSwipe();
+
+document.getElementById('btn').onclick = openPhotoSwipe;
