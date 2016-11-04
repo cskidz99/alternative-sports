71d0dbb546ac6d7b905dcb6a6c111d78dbe9495d
Cameron Skidmore <cskidz99@Camerons-MacBook-Air.local>
2016-11-04 15:45:36 -0600

    Masonry brick animations working

---
+++ js/directives/animation.js
@@ -0,0 +1,18 @@
+angular.module('altSprts').directive('animations',function(){
+  return {
+    restrict: 'EA',
+    link: function(scope,elem,attr){
+      $(window).on('scroll', function() {
+        winScroll = $(this).scrollTop();
+        console.log(winScroll);
+
+        if (winScroll > 0 && winScroll < 105) {
+        $('.masonry-brick').css({
+          'transform': 'matrix(' + winScroll/100 + ', 0, 0, ' + winScroll/100 +', 0, 0)',
+          'opacity': winScroll/100
+        });
+        }
+      })
+    }
+  }
+})
